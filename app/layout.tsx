d0f6e92171bbb169aaa404758d4e7b4b4af3f97f
tag-sap <lyr.ami.tag@gmail.com>
2026-07-19 02:16:08 +0300

    feat: добавлена Яндекс Метрика, обновлены компоненты, финальные правки

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,45 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru" className={`dark bg-background ${inter.variable} ${playfair.variable}`}>
+      <head>
+        {/* Yandex.Metrika counter */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {
+                  if (document.scripts[j].src === r) { return; }
+                }
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+                k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=110853057', 'ym');
+
+              ym(110853057, 'init', {
+                ssr:true,
+                webvisor:true,
+                clickmap:true,
+                ecommerce:"dataLayer",
+                referrer: document.referrer,
+                url: location.href,
+                accurateTrackBounce:true,
+                trackLinks:true
+              });
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            <img
+              src="https://mc.yandex.ru/watch/110853057"
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+            />
+          </div>
+        </noscript>
+        {/* /Yandex.Metrika counter */}
+      </head>
       <body className="font-sans antialiased">
         {children}
       </body>
